01c2588f6a60bdd1d66bc067859969068d7b72c0
lmcminis <leigh.mcminis@gmail.com>
2026-06-07 16:56:16 -0400

    roller subsystem

--- src/main/java/frc/robot/subsystems/belts/BeltConstants.java
+++ src/main/java/frc/robot/subsystems/belts/BeltConstants.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.belts;
 
 public class BeltConstants {
+    public static final int kMotorID = 0;
+
+    // Current limits
+    public static final int kCurrentLimit = 20;
+
+    public static final boolean kMotorInverted = false;
+
+    // Gear ratio
+    public static final double kGearRatio = 0; //find
+
+    public static final double kPositionFactor = 2 * Math.PI;
+    public static final double kVelocityFactor = kPositionFactor / 60;
+    public static final double kCruiseVelocity = 0;
+    public static final double kMaxAcceleration = 0;
     
 }
--- src/main/java/frc/robot/subsystems/belts/BeltIO.java
+++ src/main/java/frc/robot/subsystems/belts/BeltIO.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.belts;
 
-public class BeltIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface BeltIO {
+    @AutoLog
+    public static class BeltIOInputs {
+        public double motorAppliedVoltage = 0.0;
+
+        public double motorCurrentAmps = 0.0;
+
+        public double motorVelocityRadPerSec = 0.0;
+
+        public double motorTempDegC = 0.0;
+    }
+
+    public abstract void setVoltage(double volts);
     
+    public abstract void updateInputs(BeltIOInputs inputs);
 }
